f13e68051836944a51d5700a3d33cc45737af4aa
Abhishek-M-K <khandareabhishek98@gmail.com>
2023-12-14 16:19:41 +0530

    ongoing: manage members done

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,122 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+    req:Request,
+    {params}:{params:{memberId:string}}
+){
+    try{
+        const profile=await currentProfile();
+        //search server id
+        const {searchParams}=new URL(req.url);
+        const serverId=searchParams.get("serverId");
+
+        if(!profile){
+            return new NextResponse("Unauthorized", {status:401})
+        }
+
+        if(!serverId){
+            return new NextResponse("Server Id Missing", {status:400})
+        }
+
+        if(!params.memberId){
+            return new NextResponse("Member Id Missing", {status:400})
+        }
+
+        const server=await db.server.update({
+            where:{
+                id:serverId,
+                profileId:profile.id
+            },
+            data:{
+                members:{
+                    deleteMany:{
+                        id:params.memberId,
+                        profileId:{
+                            not:profile.id                        
+                        }
+                    }
+                }
+            },
+            include:{
+                members:{
+                    include:{
+                        profile:true
+                    },
+                    orderBy:{
+                        role:"asc"
+                    }
+                }
+            }
+        });
+
+        return NextResponse.json(server);
+
+    }catch(err){
+        console.log("[MEMBER_ID_DELETE]", err);
+        return new NextResponse("Internal Server Error", {status:500})
+    }
+}
+
+export async function PATCH(
+    req:Request, 
+    {params}:{params:{memberId:string}}
+){
+    try{
+        const profile=await currentProfile();
+        //search server id
+        const {searchParams}=new URL(req.url);
+        const {role}=await req.json();
+        const serverId=searchParams.get("serverId");
+
+        if(!profile){
+            return new NextResponse("Unauthorized", {status:401})
+        }
+
+        if(!serverId){
+            return new NextResponse("Server Id Missing", {status:400})
+        }
+
+        if(!params.memberId){
+            return new NextResponse("Member Id Missing", {status:400})
+        }
+
+        const server=await db.server.update({
+            where:{
+                id:serverId,
+                profileId:profile.id,
+            },
+            data:{
+                members:{
+                    update:{
+                        where:{
+                            id:params.memberId,
+                            profileId:{
+                                not:profile.id
+                            }
+                        },
+                        data:{
+                            role
+                        }
+                    }
+                }
+            },
+            include:{
+                members:{
+                    include:{
+                        profile:true
+                    },
+                    orderBy:{
+                        role:"asc"
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(server)
+    }catch(err){
+        console.log("[MEMBER_ID]", err);
+        return new NextResponse("Internal Server Error", {status:500})
+    }
+}
--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import qs from "query-string";
 import {
   Dialog,
   DialogContent,
@@ -25,6 +26,8 @@ import { ScrollArea } from "../ui/scroll-area";
 import { UserAvatar } from "../user-avatar";
 import {
   Check,
+  Hammer,
+  Loader,
   MoreVertical,
   Shield,
   ShieldAlert,
@@ -33,6 +36,10 @@ import {
 } from "lucide-react";
 import { useState } from "react";
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { MemberRole } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { set } from "react-hook-form";
 
 const rolesMapping = {
   GUEST: null,
@@ -41,72 +48,143 @@ const rolesMapping = {
 };
 
 export const MembersModal = () => {
+  const router = useRouter();
   const { isOpen, onOpen, onClose, type, data } = useModal();
   const [loadingId, setLoadingId] = useState("");
 
   const { server } = data as { server: ServerWithMembersWithProfiles }; // extractinng data from modal
   const isModalOpen = isOpen && type === "Members";
 
-  return (
-    <div className="bg-white">
-      <Dialog open={isModalOpen} onOpenChange={onClose}>
-        <DialogContent className=" bg-slate-900 text-white  overflow-hidden rounded-lg">
-          <DialogHeader>
-            <DialogTitle className="text-white font-semibold px-8 py-4 text-center">
-              Manage Members
-            </DialogTitle>
-            <DialogDescription className="text-center text-slate-500 dark:text-slate-300">
-              {server?.members?.length} Members
-            </DialogDescription>
-          </DialogHeader>
+  const handleRoleChange = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+          //not necessary since url is containing
+        },
+      });
 
-          <ScrollArea className="mt-6 max-h-[420px] pr-6">
-            {server?.members?.map((member) => (
-              <div className="flex items-center gap-x-2 mb-6" key={member.id}>
-                <UserAvatar src={member.profile.imageUrl} />
-                <div className="flex flex-col gap-y-2">
-                  <div className="text-xs font-semibold flex items-center gap-x-1">
-                    {member.profile.name}
-                    {rolesMapping[member.role]}
-                  </div>
-                  <p className="text-xs text-slate-500 dark:text-slate-300/50">
-                    {member.profile.email}
-                  </p>
+      const response = await axios.patch(url, { role });
+
+      router.refresh();
+      onOpen("Members", { server: response.data });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingId("");
+    }
+  };
+
+  const handleKickMember = async (memberId: string) => {
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      const response = await axios.delete(url);
+
+      router.refresh();
+      onOpen("Members", { server: response.data });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingId("");
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className=" bg-slate-900 text-white  overflow-hidden rounded-lg">
+        <DialogHeader>
+          <DialogTitle className="text-white font-semibold px-8 py-4 text-center">
+            Manage Members
+          </DialogTitle>
+          <DialogDescription className="text-center text-slate-500 dark:text-slate-300">
+            {server?.members?.length} Members
+          </DialogDescription>
+        </DialogHeader>
+
+        <ScrollArea className="mt-6 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div className="flex items-center gap-x-2 mb-6" key={member.id}>
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-2">
+                <div className="text-xs font-semibold flex items-center gap-x-1">
+                  {member.profile.name}
+                  {rolesMapping[member.role]}
                 </div>
-                {server.profileId !== member.profileId &&
-                  loadingId !== member.id && (
-                    <div className="ml-auto">
-                      <DropdownMenu>
-                        <DropdownMenuTrigger>
-                          <MoreVertical className="w-4 h-4 text-slate-500 dark:text-slate-300/50" />
-                          <DropdownMenuContent side="left">
-                            <DropdownMenuSub>
-                              <DropdownMenuSubTrigger className="flex items-center">
-                                <ShieldQuestion className="w-4 h-4 mr-2" />
-                                <span>Role</span>
-                              </DropdownMenuSubTrigger>
-                              <DropdownMenuPortal>
-                                <DropdownMenuSubContent>
-                                  <DropdownMenuItem className="flex flex-row mt-1 text-sm">
-                                    <Shield className="w-4 h-4 mr-2 " />
-                                    Guest
-                                    {member.role === "GUEST" && (
-                                      <Check className="w-4 h-4 ml-auto " />
-                                    )}
-                                  </DropdownMenuItem>
-                                </DropdownMenuSubContent>
-                              </DropdownMenuPortal>
-                            </DropdownMenuSub>
-                          </DropdownMenuContent>
-                        </DropdownMenuTrigger>
-                      </DropdownMenu>
-                    </div>
-                  )}
+                <p className="text-xs text-slate-500 dark:text-slate-300/50">
+                  {member.profile.email}
+                </p>
               </div>
-            ))}
-          </ScrollArea>
-        </DialogContent>
-      </Dialog>
-    </div>
+              {server.profileId !== member.profileId &&
+                loadingId !== member.id && (
+                  <div className="ml-auto">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreVertical className="w-4 h-4 text-slate-500 dark:text-slate-300/50" />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent side="left">
+                        <DropdownMenuSub>
+                          <DropdownMenuSubTrigger className="flex items-center">
+                            <ShieldQuestion className="w-4 h-4 mr-2" />
+                            <span>Role</span>
+                          </DropdownMenuSubTrigger>
+                          <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                              <DropdownMenuItem
+                                onClick={() =>
+                                  handleRoleChange(member.id, "GUEST")
+                                }
+                                className="flex flex-row mt-1 text-sm"
+                              >
+                                <Shield className="w-4 h-4 mr-2 " />
+                                Guest
+                                {member.role === "GUEST" && (
+                                  <Check className="w-4 h-4 ml-auto" />
+                                )}
+                              </DropdownMenuItem>
+
+                              <DropdownMenuItem
+                                onClick={() =>
+                                  handleRoleChange(member.id, "MODERATOR")
+                                }
+                                className="flex flex-row mt-1 text-sm"
+                              >
+                                <ShieldCheck className="w-4 h-4 mr-2 " />
+                                Moderator
+                                {member.role === "MODERATOR" && (
+                                  <Check className="w-4 h-4 ml-auto" />
+                                )}
+                              </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                          </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                          onClick={() => handleKickMember(member.id)}
+                          className="flex flex-row mt-1 text-sm py-1 ml-1"
+                        >
+                          <Hammer className="w-4 h-4 mr-2" />
+                          Remove
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                )}
+              {loadingId === member.id && (
+                <Loader className="text-slate-500 dark:text-slate-300 w-4 h-4 ml-auto animate-spin" />
+              )}
+            </div>
+          ))}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
   );
 };
